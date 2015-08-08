1fd67ac757da80d94bb14f69109c46779d10b5d8
Penkich <nishijim2001@yahoo.co.jp>
2015-08-09 00:06:22 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,9 @@
 
 //ピン
 AnalogIn analogInModule(dp9);
-//AnalogIn duty(dp10);
-//DigitalOut digitalOutModule(dp18);
-//PwmOut speaker(dp18);
 DigitalOut sp(dp14);
 
 float analogInValue; //入力部品からの入力値を保存
-//float analogInDuty; //入力部品からの入力値を保存
 
 Serial pc(USBTX, USBRX);
 int sw = 0;
